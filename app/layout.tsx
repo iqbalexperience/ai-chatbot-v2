46460c161ed54d384d21be6ebe12b966e023a768
iqbal <iqbalnawaz072@gmail.com>
2024-04-22 10:57:44 -0700

    chatbot name change

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,10 @@ export const metadata = {
     ? new URL(`https://${process.env.VERCEL_URL}`)
     : undefined,
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Workengine AI Chatbot',
+    template: `%s - Workengine AI Chatbot`
   },
-  description: 'An AI-powered chatbot ',
+  description: 'Workengine AI-powered chatbot ',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
